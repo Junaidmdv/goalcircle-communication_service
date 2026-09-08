dc8a23ada36e68f121dfc3831e469dd2dcb20263
Junaidmdv <juni313786@gmail.com>
2026-09-08 23:51:57 +0530

    feat: add package for apperror handle grpc error

---
+++ pkg/apperror/domainerror.go
@@ -0,0 +1,96 @@
+package apperror
+
+import "fmt"
+
+type ErrorType int
+
+const (
+	ErrTypeUnknown ErrorType = iota
+	ErrorTypeNotFound
+	ErrorTypeInternal
+	ErrorTypeConflict
+	ErrorInvalidArguement
+	ErrorDeadlineExceed
+	ErrorUnAuthenticated
+	ErrorBadRequest
+	ErrorFailedPrecondition
+	ErrorPermissionDenied
+)
+
+type DomainError struct {
+	Type    ErrorType
+	Message string
+	Err     error
+}
+
+func (e *DomainError) Error() string {
+	if e.Err != nil {
+		return fmt.Sprintf("%s:%v", e.Message, e.Err)
+	}
+	return e.Message
+}
+
+func NewNotFoundError(msg string) *DomainError {
+	return &DomainError{Type: ErrorTypeNotFound, Message: msg}
+}
+
+func NewInternalError(msg string, err error) *DomainError {
+	return &DomainError{Type: ErrorTypeInternal, Message: msg, Err: err}
+}
+
+func NewConflictError(msg string) *DomainError {
+	return &DomainError{
+		Type:    ErrorTypeConflict,
+		Message: msg,
+	}
+}
+
+func NewValidationError(msg string) *DomainError {
+	return &DomainError{
+		Type:    ErrorInvalidArguement,
+		Message: msg,
+	}
+}
+
+func NewDeadlineExceedeError(msg string) *DomainError {
+	return &DomainError{
+		Type:    ErrorDeadlineExceed,
+		Message: msg,
+	}
+}
+
+func NewUnAuthenticatedError(msg string) *DomainError {
+	return &DomainError{
+		Type:    ErrorUnAuthenticated,
+		Message: msg,
+	}
+
+}
+
+func NewBadRequestError(msg string) *DomainError {
+	return &DomainError{
+		Type:    ErrorBadRequest,
+		Message: msg,
+	}
+}
+
+func NewFailedPreCondition(msg string) *DomainError {
+	return &DomainError{
+		Type:    ErrorFailedPrecondition,
+		Message: msg,
+	}
+}
+
+func NewPermissionDeniedError(msg string) *DomainError {
+	return &DomainError{
+		Type:    ErrorPermissionDenied,
+		Message: msg,
+	}
+}
+
+func NewInvalidArgumentError(msg string) *DomainError {
+	return &DomainError{
+		Type:    ErrorInvalidArguement,
+		Message: msg,
+	}
+}
