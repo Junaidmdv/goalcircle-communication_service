b512d7f2d3e25ca3b8b30a1366c4f8ecc9d61237
Junaidmdv <juni313786@gmail.com>
2026-09-08 23:52:53 +0530

    feat:added validate package

---
+++ pkg/validater/custom_err.go
@@ -0,0 +1,53 @@
+package validater
+
+// import (
+// 	"regexp"
+
+// 	"github.com/Junaidmdv/goalcircle-team_service/internal/domain/entity"
+// 	"github.com/go-playground/validator/v10"
+// )
+
+// func phoneValidation(fl validator.FieldLevel) bool {
+// 	phone := fl.Field().String()
+
+// 	// E.164 format: + followed by 1–15 digits
+// 	regex := `^\+[1-9]\d{1,14}$`
+
+// 	match, _ := regexp.MatchString(regex, phone)
+// 	return match
+// }
+
+// func passwordValidation(fl validator.FieldLevel) bool {
+// 	password := fl.Field().String()
+
+// 	hasUpper := regexp.MustCompile(`[A-Z]`).MatchString(password)
+// 	hasLower := regexp.MustCompile(`[a-z]`).MatchString(password)
+// 	hasNumber := regexp.MustCompile(`[0-9]`).MatchString(password)
+// 	hasSpecial := regexp.MustCompile(`[@$!%*?&]`).MatchString(password)
+// 	hasMinLen := len(password) >= 8
+
+// 	return hasUpper && hasLower && hasNumber && hasSpecial && hasMinLen
+// }
+
+// func playerPositionValidator(fl validator.FieldLevel) bool {
+// 	return entity.PlayerPosition(fl.Field().String()) != entity.PositionUnspecified
+// }
+// func playerStatusValidater(fl validator.FieldLevel) bool {
+// 	return entity.PlayerStatus(fl.Field().String()) != entity.PlayerStatusInvalid
+// }
+
+// func staffRoleValidater(fl validator.FieldLevel) bool {
+// 	return entity.StaffRole(fl.Field().String()) != entity.StaffRoleUnspecified
+// }
+
+// func staffDesignationValidater(fl validator.FieldLevel) bool {
+// 	return entity.StaffDesignation(fl.Field().String()) != entity.StaffDesignationUnspecified
+// }
+
+// func teamNameValidation(fl validator.FieldLevel) bool {
+
+// 	teamName := fl.Field().String()
+// 	regex := `^[A-Za-z0-9][A-Za-z0-9 .'-]{2,49}$`
+// 	match, _ := regexp.MatchString(regex, teamName)
+// 	return match
+// }
---
+++ pkg/validater/validate.go
@@ -0,0 +1,135 @@
+package validater
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	en_translations "github.com/go-playground/validator/v10/translations/en"
+)
+
+type Validater struct {
+	vn *validator.Validate
+	ut ut.Translator
+}
+
+func NewValidater() (*Validater, error) {
+
+	enlocal := en.New()
+	uni := ut.New(enlocal, enlocal)
+	engtrans, _ := uni.GetTranslator("en")
+	validater := validator.New()
+	if err := en_translations.RegisterDefaultTranslations(validater, engtrans); err != nil {
+		return nil, err
+	}
+	// validater.RegisterValidation("phone", phoneValidation)
+	// validater.RegisterValidation("password", passwordValidation)
+	// validater.RegisterValidation("player-position", playerPositionValidator)
+	// validater.RegisterValidation("team-name", teamNameValidation)
+	// validater.RegisterValidation("player-status", playerStatusValidater)
+	// validater.RegisterValidation("staff-role", staffRoleValidater)
+	// validater.RegisterValidation("staff-desig", staffDesignationValidater)
+
+	validater.RegisterTagNameFunc(func(field reflect.StructField) string {
+		name := strings.SplitN(field.Tag.Get("json"), ",", 2)[0]
+		if name != "-" {
+			return name
+		}
+
+		return ""
+	})
+
+	validater.RegisterTranslation("eqfield", engtrans,
+		func(ut ut.Translator) error {
+			return ut.Add("eqfield", "{0} must match {1}", true)
+		},
+		func(ut ut.Translator, fe validator.FieldError) string {
+			t, _ := ut.T("eqfield", fe.Field(), fe.Param())
+			return t
+		},
+	)
+
+	validater.RegisterTranslation("phone", engtrans,
+		func(ut ut.Translator) error {
+			return ut.Add("phone", "{0} must be valid phone number", true)
+		},
+		func(ut ut.Translator, fe validator.FieldError) string {
+			t, _ := ut.T("phone", fe.Field())
+			return t
+		},
+	)
+
+	validater.RegisterTranslation("player-position", engtrans, func(ut ut.Translator) error {
+		return ut.Add("player-position", "invalid player position.", true)
+	},
+		func(ut ut.Translator, fe validator.FieldError) string {
+			t, _ := ut.T("player-position", fe.Field())
+			return t
+		},
+	)
+	validater.RegisterTranslation("player-status", engtrans, func(ut ut.Translator) error {
+		return ut.Add("player-status", "invalid player status.", true)
+	},
+		func(ut ut.Translator, fe validator.FieldError) string {
+			t, _ := ut.T("player-status", fe.Field())
+			return t
+		},
+	)
+
+	validater.RegisterTranslation("staff-role", engtrans, func(ut ut.Translator) error {
+		return ut.Add("staff-role", "Invalid staff role. Enter valid staff role.", true)
+	},
+		func(ut ut.Translator, fe validator.FieldError) string {
+			t, _ := ut.T("staff-role", fe.Field())
+			return t
+		},
+	)
+
+	validater.RegisterTranslation("staff-desig", engtrans, func(ut ut.Translator) error {
+		return ut.Add("staff-desig", "Invalid staff designation. Enter valid staff designation.", true)
+	},
+		func(ut ut.Translator, fe validator.FieldError) string {
+			t, _ := ut.T("staff-desig", fe.Field())
+			return t
+		},
+	)
+
+	validater.RegisterTranslation("password", engtrans,
+		func(ut ut.Translator) error {
+			return ut.Add("password", "password must contain uppercase, lowercase, number and special character", true)
+		},
+		func(ut ut.Translator, fe validator.FieldError) string {
+			t, _ := ut.T("password", fe.Field())
+			return t
+		},
+	)
+
+	validater.RegisterTranslation("team-name", engtrans,
+		func(ut ut.Translator) error {
+			return ut.Add("team-name", "{0}: '{1}' is not a valid team name", true)
+		},
+		func(ut ut.Translator, fe validator.FieldError) string {
+			t, _ := ut.T("team-name", fe.Field(), fmt.Sprintf("%v", fe.Value()))
+			return t
+		},
+	)
+
+	return &Validater{
+		vn: validater,
+		ut: engtrans,
+	}, nil
+}
+
+func (v *Validater) Validation(input interface{}) validator.ValidationErrorsTranslations {
+
+	err := v.vn.Struct(input)
+	if err == nil {
+		return nil
+	}
+
+	translated := err.(validator.ValidationErrors).Translate(v.ut)
+	return translated
+}
